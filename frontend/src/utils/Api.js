3990fa98872e9aff9534fa72bf0eb484b679d132
i-t <s_3i@mail.ru>
2023-07-15 08:05:48 +0300

    15.0.1 url

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'api.i-t.nomoredomains.work';
 
 function getHeaders() {
   return {
--- frontend/src/utils/apiAuth.js
+++ frontend/src/utils/apiAuth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'api.i-t.nomoredomains.work';
 
 const getJson = (res) => {
   if (res.ok) {
