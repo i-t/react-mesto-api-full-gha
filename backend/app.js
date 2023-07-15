c21271d4f43a4a3161b91cda5d2f264e688dcfcd
i-t <s_3i@mail.ru>
2023-07-15 20:53:39 +0300

    15.1.5 cors options

--- backend/app.js
+++ backend/app.js
@@ -13,12 +13,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 
 const app = express();
 
-const allowedCors = [
+const allowedCors = {
   origin: 'https://i-t.nomoredomains.work',
   //   'https://praktikum.tk',
   //   'http://praktikum.tk',
   //   'http://localhost:3001',
-];
+};
 
 app.use(cors(allowedCors));
 
